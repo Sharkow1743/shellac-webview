62915744155068a7f4c226be9c13583cf3d1b388
sharkow <sharkow1743@gmail.com>
2026-03-20 18:15:30 +0700

    init2

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+version = '0.1.0' 
+
+setup(
+    name='shellac-webview',
+    version=version,
+    packages=find_packages(),
+    install_requires=[
+        
+    ],
+)
